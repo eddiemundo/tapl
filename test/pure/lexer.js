a2287c0838d7e353e85813eddd6fc0749bc0eff3
Jon Shen <shenjonathan0@gmail.com>
2020-03-20 14:32:42 -0400

    code reorganisation, and unused code removal

---
+++ test/pure/lexer.js
@@ -0,0 +1,8 @@
+const test = require('ava')
+
+import * as lexer from '../../src/pure/lexer'
+
+test('lex', t => {
+//  t.log(lexer.lex('  (  \\  aa  .  bb  cc  (  ee  dd  )  ) ee \\x.x '))
+  t.pass()
+})
